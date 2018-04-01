737465a1c0e3ab6c648954dc0537eb050381c725
Unknown <musashi.miyamoto81@gmail.com>
2018-04-01 14:46:17 +0200

    [pre release] setup CORS + wedeploy.json + prod config

--- src/app.js
+++ src/app.js
@@ -18,10 +18,32 @@ const authentication = require('./authentication');
 
 const app = express(feathers());
 
+/**
+ * Apply CORS rules
+ * dev whitelist is large
+ * prod whitelist is narrow
+ */
+const corsWhitelist = configuration()().cors.whitelist;
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin) {
+      // self origin
+      callback(null, true);
+    } else if (corsWhitelist.indexOf(origin) !== -1 || corsWhitelist.indexOf('*') !== -1) {
+      // whitelisted origin
+      callback(null, true);
+    } else {
+      // reject in all other cases
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 // Load app configuration
 app.configure(configuration());
 // Enable CORS, security, compression, favicon and body parsing
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(compress());
 app.use(express.json());
